61bc1210285e281bd70f3660589957a31d9a9692
mun-kyeong <horse6953@naver.com>
2025-11-23 00:09:37 +0900

    feat: 적금 상품 목록 API 호출을 위한 SavingsProduct 인터페이스 및 함수 추가

---
+++ src/api/savingsProducts.ts
@@ -0,0 +1,14 @@
+import { http } from 'tosslib';
+
+export interface SavingsProduct {
+  id: string;
+  name: string;
+  annualRate: number;
+  minMonthlyAmount: number;
+  maxMonthlyAmount: number;
+  availableTerms: number;
+}
+
+export async function savingsProducts(): Promise<SavingsProduct[]> {
+  return await http.get<SavingsProduct[]>('/api/savings-products');
+}
